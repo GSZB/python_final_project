b088bb99e464aa9b4953289c45abcbad922982c7
g-szabolcs-b <s.gonczy@elsevier.com>
2025-11-15 16:36:52 +0100

    Task4: added the search implementation to the bot

--- address_book_main.py
+++ address_book_main.py
@@ -103,9 +103,25 @@ class AddressBook(UserDict):
     def add_record(self, record: Record):
         self.data[record.name.value] = record
 
-    def find(self, name):
-        return self.data.get(name)
+    def search(self, query: str):
+        query = query.lower()
+        result = []
 
+        for record in self.data.values():
+            if query in record.name.value.lower():
+                result.append(record)
+                continue
+
+            if any(query in phone.value for phone in record.phones):
+                result.append(record)
+                continue
+
+            if record.email and query in record.email.value.lower():
+                result.append(record)
+                continue
+
+        return result
+        
     def delete(self, name):
         if name in self.data:
             del self.data[name]
